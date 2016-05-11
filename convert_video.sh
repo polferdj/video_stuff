c15c0fc20e563752303fd9119448f4e281c55246
polferdj <polferdj@gmail.com>
2016-05-11 00:14:29 -0500

    adding converted folder to output

--- convert_video.sh
+++ convert_video.sh
@@ -3,5 +3,5 @@
 WATCH_FOLDER=~/Videos/to_be_converted/*
 
 for filename in $WATCH_FOLDER;do
-	transcode-video --crop detect --add-subtitle all -o "$filename+converted" $filename
+	transcode-video --crop detect --add-subtitle all -o "/converted/$filename" $filename
 done
