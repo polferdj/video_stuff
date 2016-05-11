f70489d458c4f6e247cda8ceb1aa2bf29c4d6994
polferdj <polferdj@gmail.com>
2016-05-11 00:16:17 -0500

    testing output filename

--- convert_video.sh
+++ convert_video.sh
@@ -3,5 +3,5 @@
 WATCH_FOLDER=~/Videos/to_be_converted/*
 
 for filename in $WATCH_FOLDER;do
-	transcode-video --crop detect --add-subtitle all -o "/converted/$filename" $filename
+	transcode-video --crop detect --add-subtitle all -o "output.mkv" $filename
 done
