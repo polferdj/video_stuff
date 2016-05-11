609b0129261376f0585c29d46ab74261ea7bd42f
polferdj <polferdj@gmail.com>
2016-05-11 00:24:23 -0500

    adding output folder

--- convert_video.sh
+++ convert_video.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 WATCH_FOLDER=~/Videos/to_be_converted/*
+OUTPUT_FOLDER=~/Videos/converted_output/
 
 for filename in $WATCH_FOLDER;do
-	transcode-video --crop detect --add-subtitle all -o "output.mkv" $filename
+	transcode-video --crop detect --add-subtitle all -o "$OUTPUT_FOLDER" $filename
 done
