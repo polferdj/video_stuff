952cb30dbda5b377cf6f3b6c4006fa73e6b7525a
polferdj <polferdj@gmail.com>
2016-05-11 00:07:31 -0500

    adding convert_video.sh

---
+++ convert_video.sh
@@ -0,0 +1,7 @@
+#! bin/bash
+
+WATCH_FOLDER=~/Videos/to_be_converted/*
+
+for filename in $WATCH_FOLDER;do
+	transcode-video --crop detect --add-subtitle all -o "$filename+converted" $filename
+done
