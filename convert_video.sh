1e94ea96c8851d3070402cb5f433232f1c593901
polferdj <polferdj@gmail.com>
2016-05-17 22:35:43 -0500

    added quotes to filename

--- convert_video.sh
+++ convert_video.sh
@@ -4,5 +4,5 @@ WATCH_FOLDER=~/Videos/to_be_converted/*
 OUTPUT_FOLDER=~/Videos/converted_output/
 
 for filename in $WATCH_FOLDER;do
-	transcode-video --crop detect --add-subtitle all -o "$OUTPUT_FOLDER" $filename
+	transcode-video --crop detect --add-subtitle all -o "$OUTPUT_FOLDER" "$filename"
 done
